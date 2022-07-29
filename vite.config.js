511d618944a8d4052117eb4c1587304f9dda2277
jeremyw-dobeu <jeremyw@dobeu.net>
2022-07-29 04:21:02 -0500

    2 files modified, 1 added

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,21 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import react from '@vitejs/plugin-react'
+
+// export plugins
+export default {
+  plugins: [
+    vue(),
+    vueJsx(),
+    //options are passed on to @vue/babel-plugin.jsx
+    react(),
+    
+    ({
+    
+    ]}
+    })]
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
